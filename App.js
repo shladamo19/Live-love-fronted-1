ddd0e2e2cd327fb95fffb993e83fabec9105a362
shladamo19 <aadamo1919@gmail.com>
2025-06-17 02:42:44 -0400

    Add files via upload
    Commit changes

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Live/Love Energy Dashboard</h1>
+      <p>This is your starter React app.</p>
+    </div>
+  );
+}
+
+export default App;
